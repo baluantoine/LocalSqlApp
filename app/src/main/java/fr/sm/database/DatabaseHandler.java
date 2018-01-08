37a19126fc883c9a69176d5f469bd3c56fb065bd
formation <antoine@yahoo.fr>
2018-01-08 14:25:15 +0100

    Insertion d'un contact dans SQLite

---
+++ app/src/main/java/fr/sm/database/DatabaseHandler.java
@@ -0,0 +1,35 @@
+package fr.sm.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Formation on 08/01/2018.
+ */
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "contacts";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String CONTACT_TABLE_SQL = "CREATE TABLE contacts(" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "first_name TEXT," +
+            "name TEXT NOT NULL," +
+            "email TEXT NOT NULL)";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) { //Crée la table
+        sqLiteDatabase.execSQL(CONTACT_TABLE_SQL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS contacts");//supprime la table si elle existe
+        this.onCreate(sqLiteDatabase); //on recrée la table
+    }
+}
--- app/src/main/java/sm/fr/localsqlapp/FormActivity.java
+++ app/src/main/java/sm/fr/localsqlapp/FormActivity.java
@@ -1,9 +1,16 @@
 package sm.fr.localsqlapp;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import fr.sm.database.DatabaseHandler;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -14,6 +21,32 @@ public class FormActivity extends AppCompatActivity {
     }
 
     public void onValid(View v){
+
         Button clickedButton = (Button) v;
+
+        //Récupération de la saisie de l'utilisateur
+        String name = ((EditText) findViewById(R.id.editNom)).getText().toString();
+        String firstName  = ((EditText) findViewById(R.id.editPrenom)).getText().toString();
+        String email  = ((EditText) findViewById(R.id.editMail)).getText().toString();
+
+        //Instanciation de la connexion à la BDD
+        DatabaseHandler db = new DatabaseHandler(this); //responsable de la gestion des demandes CRUD
+
+        //Définition des données à insérer
+        ContentValues insertValues = new ContentValues();
+        insertValues.put("first_name", firstName);
+        insertValues.put("name", name);
+        insertValues.put("email", email);
+
+        //insertion des données
+        try{
+            db.getWritableDatabase().insert("contacts", null, insertValues);
+            Toast.makeText(this, "Insertion OK", Toast.LENGTH_LONG).show();
+        } catch (SQLiteException ex){
+            Log.e("SQL_EXCEPTION", ex.getMessage());
+            Toast.makeText(this, "Insertion KO", Toast.LENGTH_LONG).show();
+        }
+       // Toast.makeText(this, "Réussi", Toast.LENGTH_LONG).show();
+
     }
 }
