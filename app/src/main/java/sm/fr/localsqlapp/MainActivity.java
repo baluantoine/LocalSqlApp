1cffb3a3f60999f0ad7bc7a8f3bd36a79fd41b18
formation <antoine@yahoo.fr>
2018-01-08 11:42:32 +0100

    Ajout d'un bouton dans MainActivity

--- app/src/main/java/sm/fr/localsqlapp/MainActivity.java
+++ app/src/main/java/sm/fr/localsqlapp/MainActivity.java
@@ -1,7 +1,9 @@
 package sm.fr.localsqlapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onAddContact(View view) {
+        if (view == findViewById(R.id.buttonAddContact)){ //Facultatif car la méthode onAddContact() n'a qu'un seul bouton
+            Intent FormIntent = new Intent(this,FormActivity.class);
+            startActivity(FormIntent);
+        }
     }
 }
